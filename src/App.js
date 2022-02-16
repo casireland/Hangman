ea0a026f6c0721ea8ed7f54b2417199e69702401
Ciaran Shiels <33393426+casireland@users.noreply.github.com>
2022-02-16 20:38:53 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Hangman from './components/Hangman'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hangman /> 
+    </div>
+  );
+}
+
+export default App;
